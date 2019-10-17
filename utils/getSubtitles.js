bf0fe06c0ab3add442a35bc67399b7c4d48e5fcf
Argenis Villasmil <argvil19@gmail.com>
2019-10-17 12:58:18 -0300

    remove video after processing it

--- utils/getSubtitles.js
+++ utils/getSubtitles.js
@@ -1,9 +1,11 @@
 const autosub = require('./autosub')
+const fs = require('fs')
 
 module.exports = async video => {
     const sub = await autosub({
         input: video
     })
+    fs.unlinkSync(video)
 
     return sub
 }
