ca21196951ec41cd28ac77153bf882d24119635f
Argenis Villasmil <argvil19@gmail.com>
2019-10-24 15:29:02 -0300

    add edit sub endpoint

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT
 const initDb = require('./db')
@@ -9,6 +10,7 @@ const initApp = async () => {
     const db = await initDb()
 
     app.use(cors())
+    app.use(bodyParser())
 
     app.get('/:id', async (req, res) => {
         const mediaId = req.params.id
@@ -19,6 +21,27 @@ const initApp = async () => {
         }
 
         return res.sendStatus(404)
+    })
+
+    app.post('/:id', async(req, res) => {
+        if (!req.body.subs) {
+            return res.sendStatus(400)
+        }
+
+        const mediaId = req.params.id
+        const result = await db.findOne({ mediaId })
+
+        if (!result) {
+            return res.sendStatus(404)
+        }
+
+        await db.updateOne({ mediaId }, {
+            $set: {
+                subs: req.body.subs
+            }
+        })
+
+        return res.send('OK')
     })
 
     app.listen(port, () => console.log(`Server running on port ${port}`))
