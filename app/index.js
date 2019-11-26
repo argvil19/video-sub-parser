cb62e237953fd61fe3e14bbedcd23223665bc6d4
Argenis Villasmil <argvil19@gmail.com>
2019-11-26 18:53:26 -0300

    added catch to new_video queue

--- app/index.js
+++ app/index.js
@@ -13,25 +13,33 @@ const initApp = async () => {
 
         console.log('Incoming job: ', mediaId)
 
-        const exists = await db.findOne({ mediaId })
+        try {
+            const exists = await db.findOne({ mediaId })
         
-        if (exists) {
-            console.log('Cant push duplicated mediaId')
+            if (exists) {
+                console.log('Cant push duplicated mediaId')
+                return done()
+            }
+
+            const insert = await db.insertOne({
+                mediaId: mediaId,
+                ready: false,
+                subs: ''
+            })
+
+            const video = await getVideoFromId(mediaId)
+            const subs = await getSubtitles(video)
+
+            await db.updateOne({
+                _id: insert.insertedId
+            }, { $set: { subs, ready: true } })
+        } catch(err) {
+            console.log(err)
+            await db.updateOne({
+                _id: insert.insertedId
+            }, { $set: { subs: 'WEBVTT\n\n', ready: true } })
             return done()
         }
-
-        const insert = await db.insertOne({
-            mediaId: mediaId,
-            ready: false,
-            subs: ''
-        })
-
-        const video = await getVideoFromId(mediaId)
-        const subs = await getSubtitles(video)
-
-        await db.updateOne({
-            _id: insert.insertedId
-        }, { $set: { subs, ready: true } })
 
         console.log(mediaId, ' Inserted!')
     
